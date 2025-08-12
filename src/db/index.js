4f40237300f541d7dab2fb1667cca81e9df5dd58
Laiba Hanif <laibahanif675@gmail.com>
2025-08-11 20:03:32 -0700

    add models

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB=async()=>{
     try {
         const connectionInstance=
-        await mongoose.connect(`${process.env.MONGODBB_URI}/${DB_NAME}`);
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
 
         console.log(`\nMongoDB connected successfully !! DB host :
         ${connectionInstance.connection.host}`);
